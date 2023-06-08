1c68011cac415d93f8def0b80a3e718d5f99ea7a
Theophilus Micah Bawa <theophilusmicah43@gmail.com>
2023-06-08 23:09:04 +0100

    added some lines of javascript

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ for (var i = 0; i< numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
 
-       switch (buttonInnerHTML) {
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+    });
+
+
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
         case "w": 
         var tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
 
         break;
-
         case "a": 
         var tom2 = new Audio("sounds/tom-2.mp3");
         tom2.play();
@@ -49,11 +63,15 @@ for (var i = 0; i< numberOfDrumButtons; i++) {
 
         default: console.log(buttonInnerHTML);
        }
-
-
-    });
-
-
 }
 
 
+function buttonAnimation(currentkey) {
+    var activeButton = document.querySelector("." + currentkey);
+
+    activeButton.classList.add ("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove ("pressed");
+    }, 100);
+}
